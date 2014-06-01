ed4f94c6779439ba0ef6a64789935e06ae51c771
Mayur Rokade <mayur.r@directi.com>
2014-06-01 06:00:14 +0530

    mail configuring to smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,13 +34,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :sendmail
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'mayurrokade.com',
-  #   user_name:            ENV['GMAIL_USERNAME'],
-  #   password:             ENV['GMAIL_PASSWORD'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: false  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'mayurrokade.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: false,
+    openssl_verify_mode: 'none'  }
 end
