ea7d8fdcaf0b6df4a948aeba532349dfe3dc8247
Mayur Rokade <mayur.r@directi.com>
2014-06-01 05:45:55 +0530

    mail configuring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'mayurrokade.com',
     user_name:            ENV['GMAIL_USERNAME'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
-    enable_starttls_auto: true  }
+    enable_starttls_auto: false  }
 end
